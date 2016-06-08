c3e86559e1896c30831c224c9639885ce573bb68
John Behan <behanj@gmail.com>
2016-06-08 22:00:23 +0700

    UI styling

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
-<?php $shout = 'YOH!'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Big Shout Out</title>
 
-<h1><?php echo $shout; ?></h1>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <div id="container">
+    <header>
+      <h1>Big Shout Out!</h1>
+    </header>
+    <div id="shouts">
+      <ul>
+        <li class="shout"><span>21:36 - </span>Kate: Badabing Badaboom!</li>
+        <li class="shout"><span>21:36 - </span>Kate: Badabing Badaboom!</li>
+        <li class="shout"><span>21:36 - </span>Kate: Badabing Badaboom!</li>
+        <li class="shout"><span>21:36 - </span>Kate: Badabing Badaboom!</li>
+      </ul>
+    </div>
+    <div id="input">
+      <form method="POST" action="process.php">
+        <input type="text" name="user" placeholder="Enter your name">
+        <input type="text" name="message" placeholder="Shout something">
+        <button type="submit">SEND YO' SHOUT!</button>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
